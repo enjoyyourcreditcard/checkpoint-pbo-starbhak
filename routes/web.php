b67f3a9f0f04868f69f8a84a5e8d043002d7e9d2
Adamoyy22 <adam.dwisaputra789@gmai.com>
2022-07-28 17:08:45 +0700

    tampilan login & tampilan catatan

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login', function () {
+    return view('login');
+});
+Route::get('perjalanan', function () {
+    return view('catatan-perjalanan');
+});
